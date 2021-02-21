8b779328e2580c9e5f5be90146b7dab5e3bad70b
Valery Shkrobov <cachemem@gmail.com>
2021-02-21 20:41:14 +0300

    First real functionality: reverse
    * Also updated gitignore to more correct version

--- cmd/gotelemeido/main.go
+++ cmd/gotelemeido/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+    "log"
+    "os"
+    "github.com/cachemem/GoTeleMeido/internal/reverse"
+    "github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 func main() {
-	fmt.Println("Hey there, nothing to see here yet")
+    botToken := os.Getenv("TOKEN")
+
+    bot, err := tgbotapi.NewBotAPI(botToken)
+    if err != nil {
+        log.Panic("Can not initialize bot")
+    }
+    bot.Debug = true
+    log.Printf("Authorized on account %s", bot.Self.UserName)
+
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
+
+    updates, err := bot.GetUpdatesChan(u)
+
+    for update := range updates {
+        if update.Message == nil { // ignore any non-Message Updates
+            continue
+        }
+
+        log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+        reversed_response := reverse.Reverse(update.Message.Text)
+        msg := tgbotapi.NewMessage(update.Message.Chat.ID, reversed_response)
+
+        bot.Send(msg)
+    }
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/cachemem/GoTeleMeido
 
-go 1.13
+go 1.15
 
 require (
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
